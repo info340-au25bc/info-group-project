a2f3d390c07768f1f331a3813fa04687df59c4d3
serenh7 <serenh7@cs.washington.edu>
2025-11-16 23:17:08 -0800

    made a template for app.jsx that includes our navbar so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
 import React from 'react'; //import React Component
+
+import NavBar from "./components/navbar";
+
+function App(props) {
+
+  return(
+    <div>
+      <NavBar />
+
+
+
+
+
+    </div>
+  );
+}
