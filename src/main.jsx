5498e37e6a6c815c3b7372316c23ee9535ad2280
serenh7 <serenh7@cs.washington.edu>
2025-11-17 01:26:49 -0800

    fixed the styling and added react strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
+import React from 'react'; //import React Component
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// imported our css styling from draft 1
 import './index.css'
 import App from './App.jsx'
-
-// imported our css styling from draft 1
-import './index.css';
 
 // imported BrowserRouter
 import { BrowserRouter } from 'react-router';
 
 createRoot(document.getElementById('root')).render(
-  // added BrowserRouter so we can use keep track of the current url
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
