7a28149e0decff0a98e1d9d07ebf8b649a0b1234
serenh7 <serenh7@cs.washington.edu>
2025-11-16 23:01:00 -0800

    Setting up: added the import react line, added the browserrouter, installed react-router,  added our css file from draft 1 to index.css, and created goaltracking file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// imported our css styling from draft 1
+import './index.css';
+
+// imported BrowserRouter
+import { BrowserRouter } from 'react-router';
+
 createRoot(document.getElementById('root')).render(
-  
+  // added BrowserRouter so we can use keep track of the current url
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
