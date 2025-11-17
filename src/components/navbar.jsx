987368e03339b2d2b316fce17a586ab3ff81e95b
serenh7 <serenh7@cs.washington.edu>
2025-11-16 23:16:52 -0800

    put our resuable nav bar into a file

---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'; //import React Component
+
+import { Link } from "react-router";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-left">
+        <img src="/img/BookBuddy Logo.png" alt="BookBuddy Logo" className="logo" />
+        <Link to="/" className="site-title-link"><h1 className="site-title">BookBuddy</h1></Link>
+      </div>
+      <div className="nav-right">
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/books">All Books</Link></li>
+          <li><Link to="/goals">Goals</Link></li>
+          <li><Link to="/search" className="search-icon">🔍</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
