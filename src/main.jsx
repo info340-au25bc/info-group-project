e46f49f8fc8fd5a12b4ec212c5f7f9ef9a48c817
serenh7 <serenh7@cs.washington.edu>
2025-12-04 00:50:53 -0800

    Finished doing the firebase configuration

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,25 @@ import App from './App.jsx'
 // imported BrowserRouter
 import { BrowserRouter } from 'react-router';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPA0bSty8yuUHpJzJCszQONdR4MkX4q-M",
+  authDomain: "bookbuddy-project-e5386.firebaseapp.com",
+  databaseURL: "https://bookbuddy-project-e5386-default-rtdb.firebaseio.com",
+  projectId: "bookbuddy-project-e5386",
+  storageBucket: "bookbuddy-project-e5386.firebasestorage.app",
+  messagingSenderId: "1000459285562",
+  appId: "1:1000459285562:web:5d85e18edf73bcdc19b103"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
